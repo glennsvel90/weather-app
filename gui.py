f818533a30cee6a43a924dd2c4cad0ef563e9d84
glennsvel90 <glennselwynvel@gmail.com>
2018-05-13 06:48:21 -0400

    stringvar

--- gui.py
+++ gui.py
@@ -35,6 +35,84 @@ helpMenu.add_command(label="About")
 helpMenu.add_cascade(label="Help", menu=helpMenu)
 
 # Add Tab Control
+tabControl = ttk.Notebook(win)
+
+tab1 = ttk.Frame(tabControl)
+tabControl.add(tab1, text="Tab 1")
+
+tab2 = ttk.Frame(tabControl)
+tabControl.add(tab2, text="Tab 2")
+
+tabControl.pack(expand=1, fill="both")
+
+weather_conditions_frame = ttk.LabelFrame(tab1, text="Current Weather Conditions")
+weather_conditions_frame.grid(column=0, row=0, padx=8, pady=4)
+
+ttk.Label(weather_conditions_frame, text="Locations").grid(column=0, row=0, sticky="W")
+
+city = tk.StringVar()
+citySelected = ttk.Combobox(weather_conditions_frame, width=12, textvariable=city)
+citySelected["values"]=("Los Angeles", "London", "Rio de Janeiro, Brazil")
+citySelected.grid(column=1, row=0)
+citySelected.current(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
